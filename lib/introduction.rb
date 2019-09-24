f3c6fb6cfbfda9be2eeff05bfe5f9ced517187c8
Anika Christopher <anika.mchristopher@gmail.com>
2019-09-24 02:56:15 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -14,7 +14,7 @@ def introduction_with_language_optional (name, language)
   puts "Hi, my name is #{name} and I am learning to program in #{language} language."
 end
 
-introduction_with_language("Josh", "Ruby")
+introduction_with_language("Josh")
 
 
 

662d1485b792b510d8a546091650c837a5a42928
Anika Christopher <anika.mchristopher@gmail.com>
2019-09-24 02:33:45 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,6 +1,9 @@
 def introduction (name)
   puts "Hi, my name is #{name}."
 end
+
+introduction ("Josh")
+
 def introduction_with_language (name) (language)
   puts "Hi, my name is #{name}, #{Ruby.rb}."
 end
